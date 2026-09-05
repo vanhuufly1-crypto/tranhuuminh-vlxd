27bfb447516efc6eecd0375e089ec46b8d5aa62b
Mây <vanhuufly@gmail.com>
2026-09-06 06:11:27 +0700

    Sync nhà cung cấp Chủ nhật 06/09: thêm sản phẩm Munich mới EP17, Primer Water, Latex S; đổi tên theo hãng Clear/Luxury Primer/Eco Happy Color; sửa mô tả A135 (keo sữa PVA); thêm HD thi công PU S700 sàn mái & Repair G50

--- js/api-config.js
+++ js/api-config.js
@@ -1,3 +1,3 @@
 // 🌐 API Configuration for Trần Hữu Minh website
 // Auto-updated by start-tunnel.sh
-var API_URL = 'https://leslie-acquire-detroit-wyoming.trycloudflare.com';
+var API_URL = 'https://messaging-ethical-speaks-studies.trycloudflare.com';
